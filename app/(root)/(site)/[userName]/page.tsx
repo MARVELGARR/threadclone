0cb303d5b96a908abe11e3e71b297f1ccdfaac31
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-05-26 15:36:51 +0100

    working on user profile

--- app/(root)/(site)/[userName]/page.tsx
+++ app/(root)/(site)/[userName]/page.tsx
@@ -1,6 +1,15 @@
-const ProfilePage = () => {
+
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import getCurrentUser from "@/hooks/getCurrentUser";
+import { getServerSession } from "next-auth";
+
+
+const ProfilePage = async () => {
+    const session = await getServerSession(authOptions)
+
+    const currentUser = await getCurrentUser(session?.data?.user.id)
     return (
-        <div className="">dsaaaaaa</div>
+        <div className="">{currentUser.name}</div>
     );
 }
  
--- hooks/getCurrentUser.tsx
+++ hooks/getCurrentUser.tsx
@@ -1,10 +1,19 @@
+import { User } from '@/util/types';
+import axios from 'axios';
 
+const getCurrentUser = async (userId: string | undefined): Promise<User> => {
+    try {
+        const user = await axios({
+            method: 'GET',
+            url: `/api/${userId}`,
+            responseType: 'json'
+        });
 
-const getCurrentUser = async (userId: string) => {
-
-    const user = await axio
-
-    return {};
+        return user.data;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        throw error;
+    }
 }
- 
+
 export default getCurrentUser;
