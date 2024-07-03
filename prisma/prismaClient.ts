8d24b29657d707ccec83e2774a4919482e85dad6
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-07-03 15:55:35 +0100

    sasa

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,17 +1,8 @@
-// prismaClient.ts
+// lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as typeof globalThis & { prisma?: PrismaClient };
 
-let prisma: PrismaClient;
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient();
-  }
-  prisma = globalForPrisma.prisma;
-}
-
-export { prisma };
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
