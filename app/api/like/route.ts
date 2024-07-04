1475ff20acf849aee045d3b58dcb46e930db97f8
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-07-04 12:41:57 +0100

    ddd

--- app/api/like/route.ts
+++ app/api/like/route.ts
@@ -1,19 +1,19 @@
 import { NextResponse } from "next/server";
-import  {prisma}  from "@/prisma/prismaClient";
-
+import { prisma } from "@/prisma/prismaClient";
 
 export async function POST(req: Request) {
     const content = await req.json();
     const { postId, userId } = content;
 
     try {
-        const findPostLike = await prisma.like.findMany({
+        const findPostLike = await prisma.like.findFirst({
             where: {
                 postId,
                 userId,
             }
         });
-        if (!findPostLike || findPostLike.length < 1) {
+
+        if (!findPostLike) {
             const newPostLike = await prisma.like.create({
                 data: {
                     postId,
@@ -21,74 +21,66 @@ export async function POST(req: Request) {
                     status: 'liked'
                 }
             });
+
             if (newPostLike) {
                 return NextResponse.json({ message: 'liked' }, { status: 200 });
             } else {
                 return NextResponse.json({ message: 'not liked' }, { status: 400 });
             }
+        } else if (findPostLike.status === 'liked') {
+            return NextResponse.json({ message: 'Already liked' });
         } else {
-            const statusRecord = findPostLike.find((item) => item.status === 'liked');
-            if (statusRecord) {
-                return NextResponse.json({ message: 'Already liked' });
-            } else {
-                const updatePostLike = await prisma.like.updateMany({
-                    where: {
-                        postId,
-                        userId,
-                    },
-                    data: {
-                        status: 'liked'
-                    }
-                });
-                if (updatePostLike && updatePostLike.count > 0) {
-                    return NextResponse.json({ message: 'post liked' }, { status: 200 });
-                } else {
-                    return NextResponse.json({ message: 'not found' }, { status: 400 });
+            const updatePostLike = await prisma.like.update({
+                where: {
+                    id: findPostLike.id,
+                },
+                data: {
+                    status: 'liked'
                 }
+            });
+
+            if (updatePostLike) {
+                return NextResponse.json({ message: 'post liked' }, { status: 200 });
+            } else {
+                return NextResponse.json({ message: 'not found' }, { status: 400 });
             }
         }
     } catch (err) {
         return NextResponse.json({ message: 'Something went wrong' }, { status: 500 });
     }
 }
-export async function PATCH(req: Request){
 
+export async function PATCH(req: Request) {
     const content = await req.json();
-    const {postId, userId} = content
+    const { postId, userId } = content;
 
-    try{
-        const newPostLike = await prisma.like.findMany({
-            where:{
+    try {
+        const findPostLike = await prisma.like.findFirst({
+            where: {
                 postId,
                 userId,
             }
-        })
-        const thePost = newPostLike?.find((post)=>post.status === 'liked')
-        if(thePost && thePost.status === 'liked'){
+        });
+
+        if (findPostLike && findPostLike.status === 'liked') {
             const updatePostLike = await prisma.like.update({
-                where:{
-                    postId,
-                    userId,
-                    
+                where: {
+                    id: findPostLike.id,
                 },
-                data:{
+                data: {
                     status: 'unliked'
                 }
-            })
-            if(updatePostLike){
-                return NextResponse.json({message: 'disliked'},{status: 200})
-            }
-            else{
-                return NextResponse.json({message: 'not found'},{status: 400})
+            });
 
+            if (updatePostLike) {
+                return NextResponse.json({ message: 'disliked' }, { status: 200 });
+            } else {
+                return NextResponse.json({ message: 'not found' }, { status: 400 });
             }
+        } else {
+            return NextResponse.json({ message: 'not liked' }, { status: 400 });
         }
-        else{
-            NextResponse.json({message: 'not liked'},{status: 400})
-
-        }
-    }
-    catch(err){
-        return NextResponse.json({message: ' Something went wrong'}, {status: 500})
+    } catch (err) {
+        return NextResponse.json({ message: 'Something went wrong' }, { status: 500 });
     }
 }
