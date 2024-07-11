51afef67867839365cd4fd0e2e27e24adb84a10f
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-07-11 20:34:46 +0100

    and finished the route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -9,12 +9,15 @@ export async function GET(req: Request){
     try{
         const comments = await prisma.reply.findMany({
             where:{
-                userId: params.userId
+                userId: userId
             },
             include: {
                 user: true
             }
         })
+        if(comments){
+            return NextResponse.json({message: "Comments gotten"},{status: 200})
+        }
     }
     catch(error){
         return NextResponse.json({message: error}, {status: 500})
