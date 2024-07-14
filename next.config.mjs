4cc89ebe9dc4bc03824cf900607343ff7aeebba6
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-07-14 16:59:55 +0100

    added uploadthings to images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['utfs.io', 'github.com','avatars.githubusercontent.com'],
+        domains: ['utfs.io', 'github.com','avatars.githubusercontent.com', 'uploadthing.com'],
     },
 };
 
