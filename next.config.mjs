bb42ef9f93d9ac7013606102c16483ac1d17da46
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-07-15 10:49:26 +0100

    sswsws

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
     images: {
         domains: ['utfs.io', 'github.com','avatars.githubusercontent.com', 'uploadthing.com'],
+        unoptimized: true,
     },
+
 };
 
 export default nextConfig;
