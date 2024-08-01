a8ccd2d515cd5e72ad32ab998ea64287cb3970a5
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-08-01 16:30:57 +0100

    sss

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,10 +42,10 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-            <div className="">
+            <div className="rrelative">
               <Header className='py-4 sticky top-0 bg-white z-50'/>
               <MobileNav className=" lg:hidden z-[999] bg-white sticky bottom-0"/>
-              <div className=' p-[2rem] lg:p-[0] overflow-y-auto'>
+              <div className=' p-[2rem] lg:p-[0] absolute overflow-y-auto'>
 
                 {children}
               </div>
