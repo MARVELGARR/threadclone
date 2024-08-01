f4370bf38d4a956f1cf7594b420aff30b42f0ca7
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-08-01 16:22:08 +0100

    xxx

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         />
             <div className="">
               <Header className='py-4 sticky top-0 bg-white z-50'/>
-              <MobileNav className=" lg:hidden z-[999] bg-white absolute bottom-0"/>
+              <MobileNav className=" lg:hidden z-[999] bg-white sticky bottom-0"/>
               <div className=' p-[2rem] lg:p-[0] '>
 
                 {children}
