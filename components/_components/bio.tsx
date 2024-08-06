a97e4508ce4bf55c8a3b6e9c5a9d36ae7f22ad96
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-08-06 08:33:29 +0100

    e

--- components/_components/bio.tsx
+++ components/_components/bio.tsx
@@ -27,7 +27,7 @@ const Bio = ({ data }: { data: ExtendedUser | null }) => {
                     <div className="w-full text-left font-bold text-2xl">{data?.profile?.name || <div className='italic'>no profile name</div>}</div>
                     <div className="w-full">@{data?.name}</div>
                 </div>
-                <Avatar className=" w-[30px] h-[30px] md:w-[70px] md:h-[70px]">
+                <Avatar className=" w-[50px] h-[50px] md:w-[70px] md:h-[70px]">
                     <AvatarImage src={avatarImage} />
                     <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
@@ -46,7 +46,7 @@ const Bio = ({ data }: { data: ExtendedUser | null }) => {
                     <Instagram className="w-9 h-9"/>
                 </Link>
             </div>
-            <UpdateProfileDialog id={data?.profile?.id || " "} name={data?.profile?.name || "No name"} links={data?.profile?.links || ""} bio={data?.profile?.bio || "No bio"} className="w-full font-extrabold"/>
+            <UpdateProfileDialog id={data?.profile?.id || " "} name={data?.profile?.name || "No name"} links={data?.profile?.links || ""} bio={data?.profile?.bio || "No bio"} className=" w-2/5  md:w-full font-extrabold"/>
         </div>
     );
 }
