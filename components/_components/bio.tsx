2ebe03a9d9c1e7df54568da7a7671e9dbeb76dd5
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2024-08-06 08:26:58 +0100

    d

--- components/_components/bio.tsx
+++ components/_components/bio.tsx
@@ -22,12 +22,12 @@ const Bio = ({ data }: { data: ExtendedUser | null }) => {
 
     return (
         <div className="text-wrap flex flex-col md:gap-4 flex-shrink">
-            <div className="flex items-center justify-between">
+            <div className="flex items-center p-2 md:p-0 justify-between flex-shrink">
                 <div className="flex flex-col items-center text-left">
                     <div className="w-full text-left font-bold text-2xl">{data?.profile?.name || <div className='italic'>no profile name</div>}</div>
                     <div className="w-full">@{data?.name}</div>
                 </div>
-                <Avatar className=" w-[50px] h-[50px] md:w-[70px] md:h-[70px]">
+                <Avatar className=" w-[30px] h-[30px] md:w-[70px] md:h-[70px]">
                     <AvatarImage src={avatarImage} />
                     <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
